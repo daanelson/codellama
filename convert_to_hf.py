8c5db031d8ae9621798cedf668a2a06476d246f9
Dan Nelson <dan.nelson8@gmail.com>
2023-08-24 22:54:18 +0000

    comma

--- convert_to_hf.py
+++ convert_to_hf.py
@@ -247,7 +247,7 @@ def write_model(model_path, input_base_path, model_size, safe_serialization=True
         num_hidden_layers=params["n_layers"],
         rms_norm_eps=params["norm_eps"],
         num_key_value_heads=num_key_value_heads,
-        vocab_size=32016 #hack
+        vocab_size=32016, #hack
         rope_theta=1000000,
         max_position_embeddings=16384
     )
